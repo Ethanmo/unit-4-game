ab1f3bd85c637a8a15ad9fc02cb0d44df030cde5
Ethanmo <mythface00@gmail.com>
2019-01-13 19:39:20 -0800

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,12 +8,24 @@ $(document).ready(function(){
         this.def = def;
         this.hp = hp;
     }
+    
+    /* Character.prototype.increaseAttack = function () {
+        this.dmg += dmg;
+    }; */
 
-    var aa = new Fighter('ph1', 1, 1, 1);
-    var bb = new Fighter('ph2', 1, 1, 1);
-    var cc = new Fighter('ph3', 1, 1, 1);
-    var dd = new Fighter('ph4', 1, 1, 1);
-    //console.log(dd);
+
+    function initiateGame(){
+        var caterpie = new Fighter('Caterpie', 120, 8, 15);
+        var rattata = new Fighter('Rattata', 100, 14, 5);
+        var gengar = new Fighter('Gengar', 150, 8, 20);
+        var dragonite = new Fighter('Dragonite', 180, 7, 20);
+        $('#choose').html("<img class='characterToChoose' id='caterpie' src='assets/images/Caterpie.png' alt='Caterpie'>");
+        $('#choose').append("<img class='characterToChoose' id='rattata' src='assets/images/Rattata.png' alt='Rattata'>");
+        $('#choose').append("<img class='characterToChoose' id='gengar' src='assets/images/Gengar.png' alt='Gengar'>");
+        $('#choose').append("<img class='characterToChoose' id='dragonite' src='assets/images/Dragonite.png' alt='Dragonite'>");
+    }
+
+    initiateGame();
 
     var player;
     var currentDmg;
@@ -21,7 +33,27 @@ $(document).ready(function(){
     
     //$('#choose').html($('#fighter1').attr('alt'));
 
-    $('body').on("click", '#fighter1', function(){
+    function displaySection(area){
+        $("'#" + area + "'").html("<img class='characterToChoose' id='caterpie' src='assets/images/Caterpie.png' alt='Caterpie'>");
+        $("'#" + area + "'").append("<img class='characterToChoose' id='rattata' src='assets/images/Rattata.png' alt='Rattata'>");
+        $("'#" + area + "'").append("<img class='characterToChoose' id='gengar' src='assets/images/Gengar.png' alt='Gengar'>");
+        $("'#" + area + "'").append("<img class='characterToChoose' id='dragonite' src='assets/images/Dragonite.png' alt='Dragonite'>");
+        if (area === 'choose'){
+            $('#choose').html("<img src='assets/images/" + (player.charAt(0).toUpperCase() + player.slice(1)) + ".png' alt='Caterpie'>");
+        } else if (area === 'enemies'){
+            
+        }
+    }
+
+    function attack (target){
+        target.healthPoints -= currentDmg;
+        $("#msg").html("You attacked " +
+            target.name + "for " + currentDmg + " hp.");
+        currentDmg += player.dmg;
+    };
+
+    //undisplay anything other than the chosen picture
+   /*  $('body').on("click", '#fighter1', function(){
         player = aa;
         enemies.push(bb);
         enemies.push(cc);
@@ -30,14 +62,18 @@ $(document).ready(function(){
         
         
         
-        
-        
-    });
+    }); */
 
-    //display enemies in section 2
+    $(document).on("click", ".characterToChoose", function(event){
+        player = event.target.id;
+    })
+
+
+
+    //display enemies in section 2, give each element a class 'enemies', and each a different ID
     function showEnemies(){
         enemies.forEach(function(enemy) {
-            $('#enemies').prepend($("<div>").html("<img src='assets/images/" + enemy.name + ".jpg' alt='" + enemy.name + "'>"));
+            $('#enemies').prepend($("<div>").html("<img src='assets/images/" + enemy.name + ".png' alt='" + enemy.name + "'>"));
         });
     }
 
